b59e0cdf77706013d3cdd362d804cdb5e6730df4
Kyle Li <likyle701@gmail.com>
2025-01-07 20:14:42 +0800

    added indicators and made it so that u grapple depending on your position

---
+++ scenes/outline.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var player = %Player
+
+func _process(delta: float) -> void:
+	queue_redraw()
+#change this later to self
+func _draw():
+	if player.grappled:
+		draw_circle(Vector2(0,0), 50, Color(1, 1, 1, 0.4), false)
+	
+	if Input.is_action_pressed("jump"):
+		draw_circle(Vector2(0,0), 50, Color(1, 1, 1, 0.4), false)
+		draw_circle(Vector2(cos(player.grapple_angle) * 50, sin(player.grapple_angle) * 50), 4, Color(1, 1, 1, 0.4))
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,13 +2,14 @@ extends CharacterBody2D
 
 
 @export var speed = 100
-@export var grapple_speed = 5.0
+@export var grapple_speed = 0
 const JUMP_VELOCITY = -300.0
 
 @export var radius: float = 50
 var angle : float = 0
 var grappled = false
 @onready var initial_pos = %Anchor
+var grapple_angle : float
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -18,9 +19,12 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	
+	grapple_angle = atan2(global_position.y - initial_pos.global_position.y, global_position.x - initial_pos.global_position.x)
 	if Input.is_action_pressed("lmb"):
 		#if !grappled:
 			#initial_pos = global_position
+		if !grappled:
+			angle = grapple_angle
 		circular_motion(delta)
 		grappled = true
 	
@@ -72,3 +76,4 @@ func circular_motion(delta):
 	
 	position.x = radius * x_pos + initial_pos.global_position.x
 	position.y = radius * y_pos + initial_pos.global_position.y
+	
