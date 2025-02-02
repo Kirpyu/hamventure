5a6ed78db0d494442363d9f586f52ccc120f4814
Kyle Li <likyle701@gmail.com>
2025-02-02 11:50:16 -0800

    Found bug, fixed it

--- hud/transition.gd
+++ hud/transition.gd
@@ -1,8 +1,7 @@
 extends ColorRect
 class_name TransitionScreen
-@export var target_scene : PackedScene
 var tween : Tween
-
+var target_scene : PackedScene
 func reset_tween():
 	if tween:
 		tween.kill()
@@ -18,6 +17,6 @@ func transition_screen():
 		reset_tween()
 		tween.tween_property(self, "color", Color.BLACK, 1.75).set_trans(Tween.TRANS_CUBIC)
 		tween.connect("finished", change_scene)
-	
+
 func change_scene():
 	get_tree().change_scene_to_packed(target_scene)
